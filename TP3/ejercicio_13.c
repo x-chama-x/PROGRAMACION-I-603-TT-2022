064c3a3bb9742e75db18d727d6da391f90b28855
Francisco Chiminelli <franciscofchiminelli@hotmail.com>
2022-05-27 00:40:47 -0300

    Add files via upload

--- TP3/ejercicio_13.c
+++ TP3/ejercicio_13.c
@@ -9,7 +9,57 @@ Utilizar una variable char (“S” o “N”), para decidir si quiere procesar 
 programa hecho por x_chama_x */
 
 #include <stdio.h>
+#include <string.h>
 int main (){
-
+    char FinProceso;
+    char alumno[20];
+    int calif, suma=0;
+    int noData=0; // se activa (1) si se ingresan datos
+    float prom;
+    int aprobado=0,cantAprob=0;
+    do
+    {
+        printf("ingrese s o S para procesar la informacion\n");
+        printf("ingrese n o N para seguir ingresando datos\n");
+        printf("ingrese opcion: ");
+        fflush(stdin);
+        scanf("%c",&FinProceso);
+        fflush(stdin);
+        if((FinProceso=='N')||(FinProceso=='n')){
+            noData=1;
+            printf("ingrese el nombre del alumno: ");
+            gets(alumno);
+            for (int i = 1; i < 4; i++)
+            {
+                printf("ingrese la %d calificacion: ",i);
+                scanf("%d",&calif);
+                suma=suma+calif;
+                if(i==3){
+                    prom=suma/i;
+                    if((calif>=6)&&(prom>=6)){
+                      aprobado=1;  
+                    }
+                }
+            }
+            suma=0;
+            printf("\npromedio del alumno %s: %.2f\n",alumno,prom);
+            if(aprobado==1){
+                printf("el alumno esta aprobado\n");
+                printf("\n");
+            }else{
+                printf("el alumno esta desaprobado\n");
+                printf("\n");
+            }
+        }
+        if(aprobado==1){
+            cantAprob=cantAprob+1;
+            aprobado=0;
+        }
+    } while ((FinProceso=='N')||(FinProceso=='n'));
+    if(noData==0){
+        printf("\nno se ingresaron datos de alumnos");
+    }else{
+        printf("\ncantidad de alumnos aprobados: %d",cantAprob);
+    }
     return 0;
 }
