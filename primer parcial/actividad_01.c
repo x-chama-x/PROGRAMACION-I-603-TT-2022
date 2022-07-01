76ba84531b27baaa97ffd2a232e8a94289f426fd
Francisco Chiminelli <franciscofchiminelli@hotmail.com>
2022-07-01 17:16:33 -0300

    Add files via upload

---
+++ primer parcial/actividad_01.c
@@ -0,0 +1,49 @@
+/* 01/07/2022
+actividad_01: 
+programa hecho por x_chama_x */
+
+#include <stdio.h>
+float reaccionCTES (int,int);
+int main (){
+    int pi,pf;
+    int diferencia,difMasBajo; // el que tiene mas diferencia es el mas bajo
+    int NumReacMasBajo;
+    int contReaccionCTE=0; // contador de reacciones ctes
+    const int tope = 1000; // cantidad de reacciones simuladas
+    for (int i = 1; i <= tope; i++) // i es el num de reaccion
+    {
+        printf("num reaccion: %d\n",i);
+        printf("ingresar pi: ");
+        scanf("%d",&pi);
+        printf("ingresar pf: ");
+        scanf("%d",&pf);
+        if (pi==pf)
+        {
+            contReaccionCTE=contReaccionCTE+1; //contador
+        }
+        
+        diferencia=pf-pi;
+        if (i==1)
+        {
+            difMasBajo=diferencia;
+            NumReacMasBajo=i;
+        }else if (diferencia>difMasBajo)
+        {
+            difMasBajo=diferencia;
+            NumReacMasBajo=i;
+        }
+    }
+    printf("diferencia que mas bajo: %d\n",difMasBajo);
+    printf("en el num de reaccion: %d\n",NumReacMasBajo);
+    printf("porcentaje de reacciones ctes: %.2f%%",reaccionCTES (contReaccionCTE,tope));
+    
+    return 0;
+}
+
+float reaccionCTES (int x,int y){
+    int porcent;
+    porcent=(x*100)/y;
+    return porcent;
+}
+
+
---
+++ primer parcial/actividad_02.c
@@ -0,0 +1,59 @@
+/* 01/07/2022
+actividad_02: MCD --> Mayor comun divisor
+programa hecho por x_chama_x */
+
+#include <stdio.h>
+int validarNum (int);
+int MCD (int,int);
+int main (){
+    int num1,num2;
+    do
+    {
+        printf("ingresar primer numero: ");
+        scanf("%d",&num1);
+    } while ((validarNum (num1)==-1));
+    do
+    {
+        printf("ingresar segundo numero: ");
+        scanf("%d",&num2);
+    } while ((validarNum (num2)==-1));
+
+    printf("el MCD entre el num %d y %d es: %d",num1,num2,MCD(num1,num2));
+    return 0;
+}
+
+int validarNum (int x){
+    if (x>1000)
+    {
+        printf("valor ingresado no valido\n");
+        return -1;
+    }else{
+        return x;
+    }
+}
+
+int MCD (int x, int y){
+    const int tope = ((x+y)/2);
+    int div;
+    for (int i = 1; i < tope; i++)
+    {
+        if ((x%i==0)&&(y%i==0))
+        {
+            div=i;
+        }
+    }
+    return div; 
+}
+    
+    
+
+
+
+
+
+
+
+    
+
+
+
